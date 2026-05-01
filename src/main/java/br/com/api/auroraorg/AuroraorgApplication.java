dbc454f63a61801324d4c47e79ec4c3c5fef595f
caarlosandree <caarlosandree@gmail.com>
2026-04-30 23:31:53 -0300

    feat: Adiciona classe principal da aplicação Spring Boot

---
+++ src/main/java/br/com/api/auroraorg/AuroraorgApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.auroraorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraorgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuroraorgApplication.class, args);
+    }
+
+}
